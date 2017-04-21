66e0967951011421b3e712106d673ba322e6574a
= <ammarmasud@gmail.com>
2017-04-21 13:31:58 -0400

    put angular js code in a seperate file

---
+++ app.js
@@ -0,0 +1,9 @@
+let app = angular.module('myApp', []);
+
+app.controller('myCtrl', function($scope) {
+  $scope.storeName="Jasper's Market";
+  $scope.adText="It's fig season! Not sure what to do with figs? Here's a great desert reciple to share.";
+  $scope.url="www.jaspers-market.com";
+  $scope.urlTitle="Fig Tart with Almonds";
+  $scope.urlDescription="The simplicity of this tart perfectly...";
+});
